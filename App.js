7a9ecc16acb8cffa852ac30a48b180e2cbdec0e1
Justin Chiasson <justinbchiasson@gmail.com>
2021-09-24 23:02:16 -0700

    amplify sign in / sign up

--- App.js
+++ App.js
@@ -1,40 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import * as eva from '@eva-design/eva';
-import { ApplicationProvider, TopNavigation, Text, Button } from '@ui-kitten/components';
-import { StyleSheet, View, SafeAreaView } from 'react-native';
-import { LinearGradient } from 'expo-linear-gradient';
+import { ApplicationProvider, Text } from '@ui-kitten/components';
+import { View } from 'react-native';
 import { default as theme } from './theme.json';
 import { default as mapping } from './mapping.json';
 import { useFonts } from 'expo-font';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
+import { AmplifyTheme, withAuthenticator } from 'aws-amplify-react-native';
+import HomeScreen from './app/screens/home';
 
-function IntroScreen() {
-  return (
-    <>
-    
-    <SafeAreaView style={{ flex: 0, backgroundColor: theme['color-primary-500'] }} />
-        <LinearGradient
-          colors={['#4A414A', theme['color-primary-500']]}
-          style={{ flex: 1 }}
-        >
-          <SafeAreaView style={styles.container}>
-            <TopNavigation
-              title={'Campfire'}
-              style={{ backgroundColor: theme['color-primary-500'] }}
-            />
-            <View style={styles.container}>
-              <Text category='p1'>Open up App.js to start working on your app!</Text>
-              <StatusBar style="light" />
-              <Button>Sign In</Button>
-            </View>
-          </SafeAreaView>
-        </LinearGradient>
-    </>
-    
-  )
-}
+Amplify.configure({...config, Analytics: {disabled: true}});
 
 function SignInScreen() {
   return (
@@ -44,12 +22,13 @@ function SignInScreen() {
   )
 }
 
-const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
-export default function App() {
+function App() {
   // load fonts before attempting to display them
-  const [loaded] = useFonts({
-    PlayfairDisplayBold: require('./assets/fonts/PlayfairDisplay-Bold.ttf'),
+  let [loaded] = useFonts({
+    DMSerifDisplay: require('./app/assets/fonts/DMSerifDisplay-Regular.ttf'),
+    OpenSans: require('./app/assets/fonts/OpenSans-Light.ttf')
   });
 
   // TODO: loading?
@@ -58,21 +37,42 @@ export default function App() {
   }
 
   return (
-    <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }} customMapping={mapping}>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Intro">
-          <Stack.Screen name="Intro" component={IntroScreen} />
-          <Stack.Screen name="SignIn" component={SignInScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </ApplicationProvider>
+      <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }} customMapping={mapping}>
+        <NavigationContainer>
+          <Tab.Navigator initialRouteName="Home">
+            <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+            <Tab.Screen name="SignIn" component={SignInScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ApplicationProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+const customContainer = Object.assign({}, AmplifyTheme.container, { backgroundColor: theme['color-primary-500'] });
+const customSectionHeaderText = Object.assign({}, AmplifyTheme.sectionHeaderText, { color: theme['color-primary-100'] });
+const customButton = Object.assign({}, AmplifyTheme.button, { backgroundColor: '#3C3540' });
+const customButtonText = Object.assign({}, AmplifyTheme.buttonText, { color: theme['color-primary-100'] });
+const customInput = Object.assign({}, AmplifyTheme.input, { color: theme['color-primary-300'], borderColor: theme['color-primary-300'] });
+const customInputLabel = Object.assign({}, AmplifyTheme.inputLabel, { color: theme['color-primary-100'] });
+const customSignedOutMessage = Object.assign({}, AmplifyTheme.signedOutMessage, { color: theme['color-primary-100'] });
+const customSectionFooterLink = Object.assign({}, AmplifyTheme.sectionFooterLink, { color: theme['color-primary-300'] });
+const customButtonDisabled = Object.assign({}, AmplifyTheme.buttonDisabled, { backgroundColor: '#37313A' });
+const authenticatorTheme = Object.assign({}, AmplifyTheme,
+  {
+    container: customContainer,
+    sectionHeaderText: customSectionHeaderText,
+    button: customButton,
+    buttonText: customButtonText,
+    input: customInput,
+    inputLabel: customInputLabel,
+    signedOutMessage: customSignedOutMessage,
+    sectionFooterLink: customSectionFooterLink,
+    buttonDisabled: customButtonDisabled
+  });
+
+export default withAuthenticator(App, {
+  theme: authenticatorTheme,
+  signUpConfig: {
+    hiddenDefaults: ['phone_number']
+  }
 });
---
+++ app/screens/home/index.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { default as theme } from '../../../theme.json';
+import { TopNavigation, Text, Button } from '@ui-kitten/components';
+import { StyleSheet, View, SafeAreaView } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { Auth } from 'aws-amplify';
+import { StatusBar } from 'expo-status-bar';
+
+const HomeScreen = () => {
+    const [user, setUser] = useState();
+  
+    useEffect(() => {
+      const fetchUser = async () => {
+        const { attributes } = await Auth.currentAuthenticatedUser();
+        setUser(attributes);
+      };
+  
+      fetchUser();
+    }, []);
+    console.log(user);
+  
+    return (
+      <>
+        <SafeAreaView style={{ flex: 0, backgroundColor: theme['color-primary-500'] }} />
+        <LinearGradient
+          colors={['#37313A', theme['color-primary-500']]}
+          style={{ flex: 1 }}
+        >
+          <SafeAreaView style={styles.container}>
+            <TopNavigation
+              title={'Campfire'}
+              style={{ backgroundColor: theme['color-primary-500'] }}
+            />
+            <View style={styles.container}>
+              <Text category='p1'>Welcome back, . Here's what your friends are listening to:</Text>
+              <StatusBar style='light' />
+              <Button onPress={signOut}>Sign Out</Button>
+            </View>
+          </SafeAreaView>
+        </LinearGradient>
+      </>
+    )
+  }
+
+  const signOut = () => {
+    Auth.signOut();
+  }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+  export default HomeScreen;
